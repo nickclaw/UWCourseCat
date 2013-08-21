1fcc2eb45be88dbab6ae92691ba2bc87c7adf30b
Nicholas Clawson <nickclaw@gmail.com>
2013-08-20 18:04:28 -0700

    Clean setup for curling

--- src/CC/DataBundle/Command/GetCommand.php
+++ src/CC/DataBundle/Command/GetCommand.php
@@ -23,7 +23,8 @@ class GetCommand extends ContainerAwareCommand
             ->setDescription('Greet someone')
             ->addArgument('year', InputArgument::REQUIRED, 'Term year to search from.')
             ->addArgument('quarter', InputArgument::REQUIRED, 'Term quarter to search from.')
-            ->addArgument('level', InputArgument::OPTIONAL, 'term campus college curriculum course section ')
+            ->addArgument('from', InputArgument::OPTIONAL, 'term campus college curriculum course section ')
+            ->addArgument('to', InputArgument::OPTIONAL, 'term campus college curriculum course section')
         ;
     }
 
@@ -31,207 +32,61 @@ class GetCommand extends ContainerAwareCommand
     {
         $year = $input->getArgument('year');
         $quarter = $input->getArgument('quarter');
-        $level = $input->getArgument('level');
-        if (!$level){
-            $level = 'term';
-        }
+        $from = $input->hasArgument('from')?$input->getArgument('from'):'term';
+        $to = $input->hasArgument('to')?$input->getArgument('to'):'section';
+
 
         $em = $this->getContainer()->get('doctrine')->getManager();
 
-        $term = null;
-        $campuses = null;
-        $colleges = null;
-        $curricula = null;
-        $courses = null;
-        $sections = null;
+        $returnedLast = null;
 
-        switch($level) {
+        switch ($from) {
             case 'term':
-                $output->writeln('Getting term...');
-                $term = $this->getTerm($year, $quarter, $em);
+                $returnedLast = $this->getTerm($year, $quarter, $em, $returnedLast);
+                if ($to === 'term') break;
             case 'campus':
-                $output->writeln('Getting campuses...');
-                $campuses = $this->getCampuses($year, $quarter, $em, $term);
+                $returnedLast = $this->getCampuses($year, $quarter, $em, $returnedLast);
+                if ($to === 'campus') break;
             case 'college':
-                $output->writeln('Getting colleges...');
-                $colleges = $this->getColleges($year, $quarter, $em, $campuses);
+                $returnedLast = $this->getColleges($year, $quarter, $em, $returnedLast);
+                if ($to === 'college') break;
             case 'curriculum':
-                $output->writeln('Getting curricula...');
-                $curricula = $this->getCurricula($year, $quarter, $em, $colleges);
+                $returnedLast = $this->getCurricula($year, $quarter, $em, $returnedLast);
+                if ($to === 'curriculum') break;
             case 'course':
-                $output->writeln('Getting courses...');
-                $this->getCourses($year, $quarter, $em, $curricula);
+                $returnedLast = $this->getCourses($year, $quarter, $em, $returnedLast);
+                if ($to === 'course') break;
             case 'section':
-                $output->writeln('Getting sections...');
+                $returnedLast = $this->getSections($year, $quarter, $em, $returnedLast);
+                if ($to === 'section') break;
         }
+        
         $em->flush();
     }
 
-    private function getTerm($year, $quarter, $em) {
-        $url = "https://ws.admin.washington.edu/student/v4/public/term/". $year .",". $quarter .".json";
-        // echo "<a href='$url'>$url</a><br />";
-        $json = file_get_contents($url);
-        $data = json_decode($json);
-
-        $term = new Term();
-        $term->setYear($year)
-            ->setQuarter($quarter);
-        $em->persist($term);
-
-        return $term;
+    private function getTerm($year, $quarter, $em, $parent) {
+        echo "Getting Term...\n";
     }
 
-    private function getCampuses($year, $quarter, $em, $term = null) {
-        if (!$term) {
-            $repo = $em->getRepository('CCDataBundle:Term');
-            $term = $repo->findOneBy(array('year' => $year, 'quarter' => $quarter));
-        }
-
-        $url = 'https://ws.admin.washington.edu/student/v4/public/campus.json';
-        // echo "<a href='$url'>$url</a><br />";
-        $json = file_get_contents($url);
-        $data = json_decode($json);
-
-        $campuses = [];
-        foreach($data->Campuses as $campus) {
-            // make a campus
-            $ca = new Campus();
-            $ca->setFullName($campus->CampusFullName)
-                ->setName($campus->CampusName)
-                ->setShortName($campus->CampusShortName)
-                ->setTerm($term);
-            $em->persist($ca);
-            $campuses[] = $ca;
-        }
-
-        return $campuses;
+    private function getCampuses($year, $quarter, $em, $parent) {
+        echo "campus\n";
     }
 
-    private function getColleges($year, $quarter, $em, array $campuses = null) {
-        if (!$campuses) {
-            $repo = $em->getRepository('CCDataBundle:Campus');
-            $campuses = $repo->findBy(array());
-        }
-        $colleges = [];
-        foreach($campuses as $campus) {
-            $url = 'https://ws.admin.washington.edu/student/v4/public/college.json'.
-                '?campus_short_name='. $campus->getShortName();
-            // echo "<a href='$url'>$url</a><br />";
-            $json = file_get_contents($url);
-            $data = json_decode($json);
-
-            foreach($data->Colleges as $college) {
-                $co = new College();
-                $co->setAbbreviation($college->CollegeAbbreviation)
-                    ->setFullName($college->CollegeFullName)
-                    ->setName($college->CollegeName)
-                    ->setShortName($college->CollegeShortName)
-                    ->setCampus($campus);
-                $em->persist($co);
-                $colleges[] = $co;
-            }
-        }
-
-        return $colleges;
+    private function getColleges($year, $quarter, $em, $parent) {
+        echo "college\n";
     }
 
-    private function getCurricula($year, $quarter, $em, array $colleges = null) {
-        if (!$colleges) {
-            $repo = $em->getRepository('CCDataBundle:College');
-            $colleges = $repo->findBy(array());
-        }
-
-        $returner = [];
-        foreach ($colleges as $college) {
-            $url = 'https://ws.admin.washington.edu/student/v4/public/curriculum.json'.
-                '?year='.$college->getCampus()->getTerm()->getYear().
-                '&quarter='.$college->getCampus()->getTerm()->getQuarter().
-                '&department_abbreviation='. rawurlencode($college->getAbbreviation()) .
-                '&sort_by=on';
-            // echo "<a href='$url'>$url</a><br />";
-            $json = file_get_contents($url);
-            $part1 = json_decode($json);
-
-            $url = 'https://ws.admin.washington.edu/student/v4/public/curriculum.json'.
-                '?year='.$college->getCampus()->getTerm()->getYear().
-                '&quarter='.$college->getCampus()->getTerm()->getQuarter().
-                '&college_abbreviation='. rawurlencode($college->getAbbreviation()) .
-                '&sort_by=on';
-            $json = file_get_contents($url);
-            $part2 = json_decode($json);
-
-            $curricula = $this->mergeCurriculum($part1->Curricula, $part2->Curricula);
-
-            foreach($curricula as $curriculum) {
-                $cu = new Curriculum();
-                $cu->setAbbreviation($curriculum->CurriculumAbbreviation)
-                    ->setFullName($curriculum->CurriculumFullName)
-                    ->setName($curriculum->CurriculumName)
-                    ->setCollege($college);
-                $em->persist($cu);
-                $returner[] = $cu;
-            }
-        }
-        return $returner;
+    private function getCurricula($year, $quarter, $em, $parent) {
+        echo "curriculum\n";;
     }
 
-    private function getCourses($year, $quarter, $em, $curricula) {
-        if (!$curricula) {
-            $repo = $em->getRepository('CCDataBundle:Curriculum');
-            $curricula = $repo->findBy(array());
-        }
-
-        $courses = [];
-        foreach($curricula as $curriculum) {
-            $url = 'https://ws.admin.washington.edu/student/v4/public/course.json'.
-                    '?year='.$curriculum->getCollege()->getCampus()->getTerm()->getYear().
-                    '&quarter='.$curriculum->getCollege()->getCampus()->getTerm()->getQuarter().
-                    '&future_terms=0'.
-                    '&curriculum_abbreviation='.rawurlencode($curriculum->getAbbreviation()).
-                    '&page_size=50';
-            do {
-                echo $url."\n";
-                $json = file_get_contents($url);
-                $data = json_decode($json);
-
-                foreach($data->Courses as $course) {
-                    $courseUrl = 'https://ws.admin.washington.edu/student/v4/public/course/'.
-                        $curriculum->getCollege()->getCampus()->getTerm()->getYear().','.
-                        $curriculum->getCollege()->getCampus()->getTerm()->getQuarter().','.
-                        rawurlencode($curriculum->getAbbreviation()).','.
-                        $course->CourseNumber.'.json';
-                    echo "    ".$courseUrl."\n";
-                    $courseJson = file_get_contents($courseUrl);
-                    $course = json_decode($courseJson);
-
-                    $crs = new Course();
-                    $crs->setNumber($course->CourseNumber)
-                        ->setTitle($course->CourseTitle)
-                        ->setLongTitle($course->CourseTitleLong)
-                        ->setCurriculum($curriculum)
-                        ->setDescription($course->CourseDescription)
-                        ->setCreditControl($course->CreditControl)
-                        ->setGradingSystem($course->GradingSystem)
-                        ->setMaxCredits($course->MaximumCredit)
-                        ->setMaxTermCredits($course->MaximumTermCredit)
-                        ->setMinTermCredits($course->MinimumTermCredit)
-                        ->setGenEd($course->GeneralEducationRequirements);
-                    $em->persist($crs);
-
-                    $courses[] = $course;
-                }
-                if ($data->Next !== null) {
-                    $url = 'https://ws.admin.washington.edu'.$data->Next->Href;
-                } else {
-                    $url = null;
-                }
-            } while ($url !== null);
-        }
-        return $courses;
+    private function getCourses($year, $quarter, $em, $parent) {
+        echo "course\n";
     }
 
-
-
+    private function getSections($year, $quarter, $em, $parent) {
+        echo "section\n";
+    }
 
     private function mergeCurriculum($a, $b) {
         foreach($a as $aa) {
